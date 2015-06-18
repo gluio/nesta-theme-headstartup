fb407f087cb3eb4033f0d43d500d02aec919d2f1
Glenn Gillen <me@glenngillen.com>
2015-06-18 21:16:48 +1000

    See if we can load refills from load path.

--- lib/nesta-theme-headstartup/app.rb
+++ lib/nesta-theme-headstartup/app.rb
@@ -1,13 +1,12 @@
 require "bourbon"
 require "neat"
 require "sinatra/flash"
-require "nesta/app"
 require "nesta-theme-headstartup/helpers"
 require "nesta-theme-headstartup/page"
 require "nesta-theme-headstartup/routes"
 
 module Nesta
-  class App
+  class App < Sinatra::Base
     helpers Nesta::Theme::Headstartup::Helpers
     include Nesta::Theme::Headstartup::Routes
   end
