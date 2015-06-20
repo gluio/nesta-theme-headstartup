f39b90181d716a537911ed2d4ffe2c1186f13da8
Glenn Gillen <me@glenngillen.com>
2015-06-20 13:08:17 +1000

    Use common library for adding to Sass path.

--- lib/nesta-theme-headstartup.rb
+++ lib/nesta-theme-headstartup.rb
@@ -11,10 +11,8 @@ end
 
 base_path = File.expand_path("..", File.dirname(__FILE__))
 view_path = File.expand_path(base_path + "/views")
-# TODO: Reload Sass loadpaths if this is already set
-STDOUT.puts("Headstartup: #{view_path}")
-ENV["SASS_PATH"] = [ENV["SASS_PATH"], view_path].compact.join(File::PATH_SEPARATOR)
 Nesta::ContentFocus::Paths.add_public_path(File.expand_path(base_path + "/public"))
 Nesta::ContentFocus::Paths.add_public_path(File.expand_path(base_path + "/public/headstartup"))
 Nesta::ContentFocus::Paths.add_view_path(view_path)
 Nesta::ContentFocus::Paths.add_view_path(File.expand_path(view_path + "/headstartup"))
+Nesta::ContentFocus::Paths.add_sass_path(view_path)
