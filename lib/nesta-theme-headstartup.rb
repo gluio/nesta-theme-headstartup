961bffaff9b452d9c0f0de4fbe79f59e74a7677b
Glenn Gillen <me@glenngillen.com>
2015-07-01 12:03:59 +1000

    DRY theme setup.

--- lib/nesta-theme-headstartup.rb
+++ lib/nesta-theme-headstartup.rb
@@ -9,7 +9,5 @@ module Nesta
   end
 end
 
-asset_path = File.expand_path("../assets", File.dirname(__FILE__))
-view_path = File.expand_path(asset_path + "/views")
-stylesheet_path = File.expand_path(asset_path + "/stylesheets")
-Nesta::Theme.register(:headstartup, { views: view_path, styles: stylesheet_path })
+base_path = File.expand_path("../assets", File.dirname(__FILE__))
+Nesta::Theme.register(:headstartup, { base: base_path })
